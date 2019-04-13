086d4d3e871cadfef7e02e20f6fd96c9522b68c8
zecoxao <zecoxao@hotmail.com>
2019-04-13 14:31:18 +0100

    quickhen fix 3

--- quickhen/common_utils.js
+++ quickhen/common_utils.js
@@ -122,3 +122,12 @@ var fail = function () {
     alert.apply(null, arguments);
     throw "fail";
 }
+
+    
+function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds)
+			break;
+	}
+}
